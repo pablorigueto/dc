428fc396bbc3171006b7c24a2d04ce702e19e017
p1 <p1@p1s-MacBook-Pro.local>
2023-09-08 18:44:36 -0300

    Create language switcher module and redirect.

---
+++ web/modules/custom/language_switcher/js/lang_country.js
@@ -0,0 +1,77 @@
+(function ($, Drupal, once) {
+
+  Drupal.behaviors.langMenuCustomBehavior = {
+    attach: function (context, settings) {
+      // Check if localStorage supports the selectedLanguage key.
+      if (typeof localStorage !== 'undefined' && localStorage.getItem('selectedLanguage')) {
+        // Retrieve the selectedLanguage value from localStorage.
+        let language = localStorage.getItem('selectedLanguage');
+
+        // Set the value as a class for the nav#block-dc-theme-language-menu element.
+        $('#block-dc-theme-language-menu', context).addClass(language + '__ls');
+        $("h2#block-dc-theme-language-menu").text(language.toUpperCase());
+
+      }
+
+      $(document).ready(function() {
+        // Loop through each anchor tag and change its href based on the title attribute.
+        $('#block-dc-theme-language ul.menu li a').each(function() {
+          let title = $(this).attr('title');
+          // Assuming you have some logic to determine the new href based on the title.
+          const NEWHREF = '/' + title;
+          $(this).attr('href', NEWHREF);
+        });
+      });
+
+    }
+  };
+
+  Drupal.behaviors.storageLangCodeOnBrowser = {
+    attach: function (context, settings) {
+      once('storageLangCodeOnBrowser', '#block-dc-theme-language .menu__item--link', context).forEach(element => {
+        element.addEventListener('click', function (e) {
+          const LANGUAGE = $(this).find(".menu__link--link").prop('title');
+          let selectedLanguageCookie = getCookie("selectedLanguage");
+
+          // Remove the existing selectedLanguage cookie (if it exists)
+          // and if they are the same, we don't need to change it.
+          if (selectedLanguageCookie === '/' + LANGUAGE) {
+            return;
+          }
+
+          deleteCookie("selectedLanguage");
+  
+          // Set the new value in both localStorage and as a new cookie
+          //localStorage.setItem('selectedLanguage', LANGUAGE);
+          setCookie("selectedLanguage", '/' + LANGUAGE, 365);
+
+        });
+      });
+    }
+  };
+
+  function setCookie(name, value, daysToExpire) {
+    const expirationDate = new Date();
+    expirationDate.setTime(expirationDate.getTime() + (daysToExpire * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + expirationDate.toUTCString();
+    document.cookie = name + "=" + value + "; " + expires + "; path=/";
+  }
+
+  // Function to delete a cookie
+  function deleteCookie(name) {
+    document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/';
+  }
+
+  // Function to get a cookie by name
+  function getCookie(name) {
+    const cookies = document.cookie.split('; ');
+    for (const cookie of cookies) {
+      const [cookieName, cookieValue] = cookie.split('=');
+      if (cookieName === name) {
+        return cookieValue;
+      }
+    }
+    return null;
+  }
+
+})(jQuery, Drupal, once);
---
+++ web/modules/custom/language_switcher/src/EventSubscriber/RedirectSubscriber.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Drupal\language_switcher\EventSubscriber;
+
+use Drupal\Core\Cache\CacheableRedirectResponse;
+use Drupal\Core\Language\LanguageManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+//use Drupal\Core\Routing\TrustedRedirectResponse;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * Redirects to langcode if the path didn't have the / at the end.
+ */
+class RedirectSubscriber implements EventSubscriberInterface {
+
+  /**
+   * The Language Manager Interface.
+   *
+   * @var Drupal\Core\Language\LanguageManagerInterface
+   */
+  protected $languageManager;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(LanguageManagerInterface $languageManager) {
+    $this->languageManager = $languageManager;
+  }
+
+  // /**
+  //  * {@inheritdoc}
+  //  */
+  // public static function create(ContainerInterface $container): self {
+  //   return new static(
+  //     $container->get('language_manager'),
+  //   );
+  // }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events[KernelEvents::REQUEST][] = ['onRequest'];
+    return $events;
+  }
+
+  /**
+   * Limit this solution only to homepage.
+   */
+  public function onRequest(RequestEvent $event) {
+    $path = $event->getRequest()->getPathInfo();
+    $cookie = $event->getRequest()->cookies->get('selectedLanguage');
+
+    $defaultLanguage = $this->getDefaultLanguage();
+
+    // If path is default language on the first access.
+    if ($path === $defaultLanguage && empty($cookie)) {
+      return;
+    }
+
+    if (empty($cookie)) {
+      return;
+    }
+
+    if ($path === $cookie) {
+      return;
+    }
+
+    $path_parts = explode('/', $path);
+    if (isset($path_parts[2])) {
+      return;
+    }
+
+    $response = new CacheableRedirectResponse($cookie);
+    //$response = new TrustedRedirectResponse($cookie);
+    $response->getCacheableMetadata()->setCacheMaxAge(0);
+    $event->setResponse($response);
+    $event->stopPropagation();
+
+  }
+
+
+  public function getDefaultLanguage() {
+    // Get the default language object.
+    //$this->languageManager->getCurrentLanguage();
+    //$defaultLanguage = \Drupal::languageManager()->getDefaultLanguage();
+
+    $defaultLanguage = $this->languageManager->getDefaultLanguage();
+    // Get the language code of the default language.
+    return '/' . $defaultLanguage->getId();
+  }
+
+}
