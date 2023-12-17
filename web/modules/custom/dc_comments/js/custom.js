45764a3b507c32d11dde0a342096aae5f522ea18
Pablo Rigueto <pabloedurigueto@outlook.com>
2023-12-17 15:06:35 -0300

    Order comments.

---
+++ web/modules/custom/dc_comments/js/custom.js
@@ -0,0 +1,30 @@
+(function ($, Drupal, once) {
+
+  Drupal.behaviors.commentsMore = {
+    attach(context) {
+      if (!drupalSettings.commentsMore) {
+        $(document).ready(function () {
+          // Add a click event handler to the Menu heading.
+          $(".comments-more", context).click(function () {
+            // Find the related menu element within the same parent.
+            const MORE = $(this).siblings('.comment-edit, .comment__time, .comments-manage-links');
+  
+            // Check if the more element exists.
+            if (MORE.length) {
+              // Check if the elements are visible.
+              if (MORE.is(":visible")) {
+                // Use fadeOut to hide both elements.
+                MORE.fadeOut();
+              } else {
+                // Use fadeIn to show both elements.
+                MORE.fadeIn();
+              }
+            }
+          });
+        });
+        drupalSettings.commentsMore = true;
+      }
+    }
+  };
+
+})(jQuery, Drupal, once);
