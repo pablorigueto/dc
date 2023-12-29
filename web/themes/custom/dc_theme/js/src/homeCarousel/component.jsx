79f74afa456075a1cf0d285ab3fef9faf9f25d68
Pablo Rigueto <pabloedurigueto@outlook.com>
2023-12-29 18:07:34 -0300

    Swiper splited.

--- web/themes/custom/dc_theme/js/src/homeCarousel/component.jsx
+++ web/themes/custom/dc_theme/js/src/homeCarousel/component.jsx
@@ -1,16 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useQuery } from 'react-query';
 import { fetchNodes } from './controller';
 import './assets/index.css';
 import Dropdown from './dropdown';
-import { Swiper, SwiperSlide } from 'swiper/react';
-
 import getSwiperConfig from './swiperBreakPoints';
-
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
+import SwiperComponent from './swiperComponent';
 
 const NodeList = () => {
 
@@ -81,68 +75,8 @@ const NodeList = () => {
           handleSortChange={handleSortChange}
         />
       </div>
-        
-      <Swiper {...swiperConfig}>
-        {sortedNodes.map((node) => (
-          <SwiperSlide key={node.id}>
 
-            <a className="node-path" href={node.node_path}>
-
-              <div className="frontpage-result-thumbnail">
-
-                <div className="teaser-tag-group">
-                  {node.tags.map((tag) => (
-                    <div key={tag.id} className={`frontpage-label ${tag.alias.toLowerCase()}`}>
-                      {tag.alias}
-                    </div>
-                  ))}
-                </div>
-
-                <div className='frontpage-title-display'>
-                  <div className="frontpage-title">{node.title}</div>
-                    <div className="count-frontpage">
-
-                      <div className="views_node_count">
-                        {node.node_view_count}
-                        <span className="material-symbols-outlined">
-                          visibility
-                        </span>
-                      </div>
-
-                      <div className="comments_count">
-                        {node.comments_count}
-                        <span className="material-symbols-outlined">
-                          chat_bubble
-                        </span>
-                      </div>
-
-                      <div className="likes_count">
-                        {node.likes_count}
-                        <span className="material-symbols-outlined">
-                          thumb_up
-                        </span>
-                      </div>
-                    </div>
-
-                    <div className="profile-frontpage">
-
-                      <div className="profile-img-name">
-                        <img className="profile-frontpage-img" src={node.node_owner_image_profile} alt="User Profile Image" />
-                        <div className="owner-frontpage">
-                          {node.node_owner_name}
-                        </div>
-                      </div>
-
-                      {node.node_created}
-                    </div>
-
-                  </div>
-                  <img src={node.url} alt={node.alt} />
-              </div>
-            </a>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      <SwiperComponent sortedNodes={sortedNodes} swiperConfig={swiperConfig} />
 
       <div className="node-list-container">
         <div className="recent-section">
